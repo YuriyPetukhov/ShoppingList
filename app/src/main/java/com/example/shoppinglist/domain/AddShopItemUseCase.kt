5f9c0b7355ac84827c6bd4da9c6d301830c3bd9d
YuriyPetukhov <pochtalosyahs@gmail.com>
2025-03-21 22:46:02 +0300

    Added domain classes

---
+++ app/src/main/java/com/example/shoppinglist/domain/AddShopItemUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.shoppinglist.domain
+
+import android.os.Build.VERSION_CODES.S
+
+class AddShopItemUseCase {
+    fun addItem(id: Int, name: String, count: Int, enabled: Boolean) {
+
+    }
+}

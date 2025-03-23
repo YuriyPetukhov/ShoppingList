08eaa6af37f4effd962a55ff780cb662be576c3c
YuriyPetukhov <pochtalosyahs@gmail.com>
2025-03-23 15:38:38 +0300

    Added repository implementation finished

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,5 +1,35 @@
 package com.example.shoppinglist.data
 
-class ShopListRepositoryImpl {
+import com.example.shoppinglist.domain.ShopItem
+import com.example.shoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+    private val shopList = mutableListOf<ShopItem>()
+    private var autoIncrementId = 0
+    override fun addItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        deleteShopItem(oldElement)
+        addItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find { it.id == shopItemId }
+            ?: throw RuntimeException("Element with given id: $shopItemId was not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
 
 }
